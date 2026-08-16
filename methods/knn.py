b124c314f0670cf01f9eefb9dc94d881c7cd9414
Dumitru Andriuta <andriutadumitru8@gmail.com>
2026-08-16 16:04:31 +0100

    Refactor KNN functions with consistent imports

--- methods/knn.py
+++ methods/knn.py
@@ -1,7 +1,4 @@
-import networkx as nx
 import numpy as np
-from grakel import graph_from_networkx
-from grakel.kernels import WeisfeilerLehman, VertexHistogram
 from sklearn.neighbors import KNeighborsClassifier
 
 def get_predictions(distance_matrix, y, idx_train, idx_test):
