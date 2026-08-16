e981c0a27d5a82c9c5d49f55124f8c0ca353a4f3
Dumitru Andriuta <andriutadumitru8@gmail.com>
2026-08-16 15:41:46 +0100

    Refactor methods and update imports in ANOVA script

--- monte_carlo_frechet_anova.py
+++ monte_carlo_frechet_anova.py
@@ -10,7 +10,7 @@ from joblib import Parallel, delayed
 import torch
 
 from data_gen import generate_data
-from methods import kernel_svm_wl, gcn, ginestet2017, dubey2019, knn_wl, lovato2020
+from methods import kernel_svm, gcn, ginestet2017, dubey2019, knn, lovato2020
 from permutation_test import permutation_test
 
 # ----------------------------------------------------------------------
